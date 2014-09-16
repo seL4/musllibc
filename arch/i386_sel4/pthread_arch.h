6aa6001a2413db18f056daa14801e64c0c089e6f
Adrian Danis <Adrian.Danis@nicta.com.au>
2014-09-16 14:59:57 +1000

    Use fs instead of gs for tls, as defined by seL4

--- arch/i386_sel4/pthread_arch.h
+++ arch/i386_sel4/pthread_arch.h
@@ -1,1 +1,10 @@
-../i386/pthread_arch.h
+static inline struct pthread *__pthread_self()
+{
+	struct pthread *self;
+	__asm__ __volatile__ ("movl %%fs:0,%0" : "=r" (self) );
+	return self;
+}
+
+#define TP_ADJ(p) (p)
+
+#define CANCEL_REG_IP 14
