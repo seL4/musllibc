599cf3f49deb016da5d09c2c260095df0b7a1aa8
Adrian Danis <Adrian.Danis@nicta.com.au>
2015-12-02 11:28:22 +1100

    Temporary fix for __set_thread_area on arm

--- src/thread/__set_thread_area.c
+++ src/thread/__set_thread_area.c
@@ -5,6 +5,10 @@ int __set_thread_area(void *p)
 #ifdef SYS_set_thread_area
 	return __syscall(SYS_set_thread_area, p);
 #else
-	return -ENOSYS;
+    /* The __init_tls routine requires that __set_thread_area work.
+     * This function should be overriden by an arch __set_thread_area
+     * function but for reasons unknown this is not happening,
+     * consider this a temporary hack */
+    return 0;
 #endif
 }
