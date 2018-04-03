3b5c7f5adeb113cf758b3af3aa4caae0698598b5
Kent McLeod <Kent.Mcleod@data61.csiro.au>
2018-04-03 14:28:03 +1000

    RISC-V: Remove unnecessary definitions

--- arch/riscv/reloc.h
+++ arch/riscv/reloc.h
@@ -1,23 +1,7 @@
-#include <string.h>
-#include <elf.h>
-#include <endian.h>
-
 #define LDSO_ARCH "riscv"
 
 #define TPOFF_K 0
 
-static int remap_rel(int type)
-{
-  /* TODO */
-	return 0;
-}
-
+// Relocation is not currently supported for riscv
 #define CRTJMP(pc,sp) __asm__ __volatile__( \
 	".word 0x00000000" )
-
-
-#include "syscall.h"
-void __reloc_self(int c, size_t *a, size_t *dynv)
-{
-  /* TODO */
-}
