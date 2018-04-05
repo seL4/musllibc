f02a8407e53caa81dcd6a88da68c3dc29f39a340
Anna Lyons <Anna.Lyons@data61.csiro.au>
2018-04-05 12:01:11 +1000

    riscv: build on 32-bit

--- arch/riscv/bits/limits.h
+++ arch/riscv/bits/limits.h
@@ -1,8 +1,12 @@
 #if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) \
  || defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
 #define PAGE_SIZE 4096 
-#define LONG_BIT 64 
+#define LONG_BIT __riscv_xlen
 #endif
 
+#if __riscv_xlen == 32
+#define LONG_MAX  0x7fffffffL
+#else
 #define LONG_MAX  0x7fffffffffffffffL
+#endif
 #define LLONG_MAX  0x7fffffffffffffffLL
