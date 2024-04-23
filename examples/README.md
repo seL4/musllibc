40e862ea5f8f8fe4990fbcbf8882498d350736dd
Farid Zakaria <fmzakari@google.com>
2024-04-23 20:22:16 +0000

    Add examples directory

---
+++ examples/README.md
@@ -0,0 +1,19 @@
+# Examples
+
+This document outlines some of the manual steps you can go through to get this fork working.
+
+The changes in this fork are mainly restricted to [dynlink.c](../ldso/dynlink.c) to support a new relocation format optimized for systems such as Nix & Spack.
+
+## Prerequisites
+
+Compile musl and install it
+
+```console
+./configure --prefix=$(realpath build) \
+            --exec-prefix=$(realpath build) \
+            --syslibdir=$(realpath build/lib/) \
+            --host=x86_64-linux-gnu \
+            --enable-debug
+
+make && make install
+```
---
+++ examples/hello_world.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+void my_function() {
+    printf("This is my_function.\n");
+}
+
+int main() {
+    printf("Hello, World!\n");
+
+    Dl_info info;
+
+    // Getting information about the address of my_function
+    if (dladdr((void *)my_function, &info)) {
+        printf("Filename: %s\n", info.dli_fname);
+        printf("Base address of shared object: %p\n", info.dli_fbase);
+        printf("Name of nearest symbol: %s\n", info.dli_sname);
+        printf("Address of nearest symbol: %p\n", info.dli_saddr);
+    } else {
+        printf("Error occurred while retrieving information.\n");
+    }
+
+    return 0;
+}
