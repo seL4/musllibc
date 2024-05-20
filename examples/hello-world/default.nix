071f4f1a27fe2e6c6ddacd302a3a6bd56ce016b6
Farid Zakaria <fmzakari@google.com>
2024-05-20 18:18:33 +0000

    moved hello world to own directory

---
+++ examples/hello-world/default.nix
@@ -0,0 +1,39 @@
+{ stdenv, patchExecutable, lib }:
+let
+  fs = lib.fileset;
+  libfoo = stdenv.mkDerivation rec {
+    name = "libfoo";
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [ ./Makefile ./foo.c ];
+    };
+    nativeBuildInputs = [ ];
+    dontStrip = true;
+    buildPhase = ''
+      ls -l
+      ls -l $src
+      make libfoo.so
+    '';
+    installPhase = ''
+      mkdir -p $out/lib
+      cp libfoo.so $out/lib
+    '';
+  };
+  hello_world = stdenv.mkDerivation {
+    name = "hello_world";
+    src = fs.toSource {
+      root = ./.;
+      fileset = fs.unions [ ./Makefile ./hello_world.c ];
+    };
+    nativeBuildInputs = [ libfoo ];
+    dontStrip = true;
+    buildPhase = ''
+      make hello_world
+    '';
+    installPhase = ''
+      mkdir -p $out/bin
+      cp hello_world $out/bin
+    '';
+  };
+
+in patchExecutable { executable = hello_world; }
