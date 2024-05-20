30f7f3ce161a2362d40f1ae6886547268f47633a
Farid Zakaria <fmzakari@google.com>
2024-05-20 18:50:59 +0000

    split into two functions

--- patch_executable.nix
+++ patch_executable.nix
@@ -1,24 +1,55 @@
-{ stdenv, patchelf, musl }:
-{ executable, command ? "--version" }:
-stdenv.mkDerivation {
-  name = "patched_${executable.name}";
+{ stdenv, patchelf, musl, lib }: {
+  all = { name ? lib.strings.getName executable, executable
+    , command ? "--version" }:
+    stdenv.mkDerivation {
+      name = "patched_${name}";
 
-  buildInputs = [ patchelf musl executable ];
+      buildInputs = [ patchelf musl executable ];
 
-  phases = "installPhase";
+      phases = "installPhase";
 
-  installPhase = ''
-    mkdir -p $out/bin
-    # Apply patchelf to all binaries in the bin directory to set the new interpreter.
-    for bin in ${executable}/bin/*; do
-        patchelf --set-interpreter ${musl}/lib/libc.so $bin --output $out/bin/$(basename $bin)
-    done
-    # Add the custom relocation section
-    for bin in $out/bin/*; do
-        RELOC_WRITE=1 $bin ${command}
-        cp relo.bin $out/bin/$(basename $bin).relo
+      installPhase = ''
+        mkdir -p $out/bin
+        # Apply patchelf to all binaries in the bin directory to set the new interpreter.
+        for bin in ${executable}/bin/*; do
+            # Check if the file is executable
+            if [[ ! -x "$bin" ]]; then
+                continue
+            fi
+
+            # Check if the file is an ELF binary
+            if ! file "$bin" | grep -q "ELF"; then
+                continue
+            fi
+            patchelf --set-interpreter ${musl}/lib/libc.so $bin --output $out/bin/$(basename $bin)
+        done
+        # Add the custom relocation section
+        for bin in $out/bin/*; do
+            RELOC_WRITE=1 $bin ${command}
+            cp relo.bin $out/bin/$(basename $bin).relo
+            objcopy --add-section .reloc.cache=relo.bin \
+                --set-section-flags .reloc.cache=noload,readonly $bin
+        done
+      '';
+    };
+
+  individual = { name ? lib.strings.getName executable, executable
+    , command ? "--version" }:
+    stdenv.mkDerivation {
+      name = "patched_${name}";
+
+      buildInputs = [ patchelf musl executable ];
+
+      phases = "installPhase";
+
+      installPhase = ''
+        mkdir -p $out/bin
+        patchelf --set-interpreter ${musl}/lib/libc.so ${executable}/bin/${name} --output $out/bin/${name}
+        RELOC_WRITE=1 $out/bin/${name} ${command}
+        cp relo.bin $out/bin/${name}.relo
         objcopy --add-section .reloc.cache=relo.bin \
-            --set-section-flags .reloc.cache=noload,readonly $bin
-    done
-  '';
+                --set-section-flags .reloc.cache=noload,readonly $out/bin/${name}
+      '';
+    };
+
 }
